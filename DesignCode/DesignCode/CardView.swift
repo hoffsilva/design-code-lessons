d9a5c584d14c2093c63ffd237d9429aa5a6cad88
Hoff Silva <hoff.henry@gmail.com>
2023-03-27 19:59:07 -0400

    Create items

---
+++ DesignCode/DesignCode/CardView.swift
@@ -0,0 +1,43 @@
+//
+//  CardView.swift
+//  DesignCode
+//
+//  Created by Hoff Silva on 2023-03-27.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("UI Design")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                    Text("certificate")
+                        .foregroundColor(Color("accent"))
+                }
+                Spacer()
+                Image("Logo1")
+            }
+            .padding(20)
+            Spacer()
+            Image("Card1")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 300, height: 110, alignment: .top)
+        }
+        .frame(width: 340, height: 220)
+        .background(.black)
+        .cornerRadius(20)
+        .shadow(radius: 20)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
