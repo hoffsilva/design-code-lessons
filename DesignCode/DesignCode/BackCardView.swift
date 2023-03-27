e77645b19a0aca7a0ec516d323841d553fd7649d
Hoff Silva <hoff.henry@gmail.com>
2023-03-27 19:58:14 -0400

    Added components

---
+++ DesignCode/DesignCode/BackCardView.swift
@@ -0,0 +1,23 @@
+//
+//  BackCardView.swift
+//  DesignCode
+//
+//  Created by Hoff Silva on 2023-03-27.
+//
+
+import SwiftUI
+
+struct BackCardView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+        }
+        .frame(width: 340, height: 200)
+    }
+}
+
+struct BackCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        BackCardView()
+    }
+}
--- DesignCode/DesignCode/ContentView.swift
+++ DesignCode/DesignCode/ContentView.swift
@@ -8,40 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var show = false
+    
     var body: some View {
         ZStack {
-            VStack {
-                Spacer()
-            }
-            .frame(width: 300, height: 200)
-            .background(.blue)
-            .cornerRadius(20)
-            .shadow(radius: 20)
-            .offset(x:0, y: -20)
-            VStack {
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text("UI Design")
-                            .font(.title)
-                            .fontWeight(.semibold)
-                            .foregroundColor(.white)
-                        Text("certificate")
-                            .foregroundColor(Color("accent"))
-                    }
-                    Spacer()
-                    Image("Logo1")
+            TitleView()
+                .blur(radius: show ? 20 : 0)
+                .animation(.default)
+            
+            BackCardView()
+                .background(show ? Color("card3") : Color("card4"))
+                .cornerRadius(20)
+                .shadow(radius: 20)
+                .offset(x:0, y: show ? -400 : -40)
+                .scaleEffect(0.9)
+                .rotationEffect(.degrees(show ? 0 : 10))
+                .rotation3DEffect(.degrees(10), axis: (x: 10, y: 0, z: 0))
+                .blendMode(.hardLight)
+                .animation(.easeOut(duration: 0.5))
+            
+            BackCardView()
+                .background(show ? Color("card4") : Color("card3"))
+                .cornerRadius(20)
+                .shadow(radius: 20)
+                .offset(x:0, y: show ? -200 : -20)
+                .scaleEffect(0.95)
+                .rotationEffect(.degrees(show ? 0 : 5))
+                .rotation3DEffect(.degrees(5), axis: (x: 10, y: 0, z: 0))
+                .blendMode(.hardLight)
+                .animation(.easeOut(duration: 0.3))
+            
+            CardView()
+                .blendMode(.hardLight)
+                .onTapGesture {
+                    show.toggle()
                 }
-                .padding(20)
-                Spacer()
-                Image("Card1")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 300, height: 110, alignment: .top)
-            }
-            .frame(width: 340, height: 220)
-            .background(Color.blue)
-            .cornerRadius(20)
-            .shadow(radius: 20)
+            
+            BottomCardView()
+                .blur(radius: show ? 20 : 0)
+                .animation(.default)
         }
     }
 }
