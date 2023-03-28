49225765bed8bd4f3623e9158b0942b7265320b0
Hoff Silva <hoff.henry@gmail.com>
2023-03-28 16:07:16 -0400

    Added Drag Progress and Tap Background

--- DesignCode/DesignCode/HomeView.swift
+++ DesignCode/DesignCode/HomeView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct HomeView: View {
     
     @State var showProfile = false
+    @State var viewState = CGSize.zero
     
     var body: some View {
         ZStack {
@@ -38,15 +39,32 @@ struct HomeView: View {
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
             .shadow(color: .black.opacity(0.2), radius: 20, x: 0, y: 20)
             .offset(y: showProfile ? -450 : 0)
-            .rotation3DEffect(.degrees(showProfile ? -10 : 0), axis: (x: 10, y: 0, z: 0))
+            .rotation3DEffect(.degrees(showProfile ? (viewState.height / 10) : 0), axis: (x: 10, y: 0, z: 0))
             .scaleEffect(showProfile ? 0.9 : 1)
             .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
             .edgesIgnoringSafeArea(.all)
             
             
             MenuView()
-                .offset(y: showProfile ? 0 : 600)
+                .background(.black.opacity(0.001))
+                .offset(y: showProfile ? 0 : 1000)
+                .offset(y: viewState.height)
                 .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+                .onTapGesture {
+                    self.showProfile.toggle()
+                }
+                .gesture(
+                    DragGesture()
+                        .onChanged { vaulue in
+                            self.viewState = vaulue.translation
+                        }
+                        .onEnded { value in
+                            if self.viewState.height > 50 {
+                                self.showProfile = false
+                            }
+                            self.viewState = .zero
+                        }
+                )
         }
     }
 }
