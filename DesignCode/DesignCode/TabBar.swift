e91114f448927dc65f3235b35343d709fed4e223
Hoff Silva <hoff.henry@gmail.com>
2023-03-30 13:58:58 -0400

    Added Tab Bar and Test on Device

---
+++ DesignCode/DesignCode/TabBar.swift
@@ -0,0 +1,34 @@
+//
+//  TabBar.swift
+//  DesignCode
+//
+//  Created by Hoff Silva on 2023-03-29.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        TabView {
+            Home()
+                .tabItem {
+                    Image(systemName: "play.circle.fill")
+                    Text("Home")
+                }
+            ContentView()
+                .tabItem {
+                    Image(systemName: "rectangle.stack.fill")
+                    Text("Certificates")
+                }
+        }
+    }
+}
+
+struct TabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            TabBar().previewDevice("iPhone 8")
+            TabBar().previewDevice("iPhone 11 Pro Max")
+        }
+    }
+}
