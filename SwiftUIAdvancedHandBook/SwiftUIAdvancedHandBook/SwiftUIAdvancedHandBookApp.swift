e6805f78b1e5e22fe97aa48d94adf69725cf7dcb
Hoff Silva <hoff.henry@gmail.com>
2023-04-03 13:23:13 -0400

    Create project

---
+++ SwiftUIAdvancedHandBook/SwiftUIAdvancedHandBook/SwiftUIAdvancedHandBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAdvancedHandBookApp.swift
+//  SwiftUIAdvancedHandBook
+//
+//  Created by Hoff Silva on 2023-04-03.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAdvancedHandBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
